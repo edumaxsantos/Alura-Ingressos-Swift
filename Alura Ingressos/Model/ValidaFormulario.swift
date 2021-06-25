2739de9eba450e50f85cec1054b757ea450bb02b
edumaxsantos@gmail.com <edumaxsantos@gmail.com>
2021-06-25 00:46:58 -0300

    Adicionado validação para email

--- Alura Ingressos/Model/ValidaFormulario.swift
+++ Alura Ingressos/Model/ValidaFormulario.swift
@@ -33,6 +33,14 @@ class ValidaFormulario: NSObject {
         return true
     }
     
+    func verificaEmail(_ email: String) -> Bool {
+        let emailRegEx = "(?:[a-z0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[a-z0-9!#$%\\&'*+/=?\\^_`{|}"+"~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\"+"x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-"+"z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5"+"]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-"+"9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21"+"-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+        
+        let emailTest = NSPredicate(format: "SELF MATCHES[c] %@", emailRegEx)
+        
+        return emailTest.evaluate(with: email)
+    }
+    
     func verificaTextFieldsValidos(listaDeTextFields: [UITextField]) -> Bool {
         var dicionarioDeTextFields: [TiposDeTextField:UITextField] = [:]
         
@@ -43,6 +51,8 @@ class ValidaFormulario: NSObject {
         }
         
         guard let cpf = dicionarioDeTextFields[.cpf], BooleanValidator().validate(cpf: cpf.text!) else { return false }
+        
+        guard let email = dicionarioDeTextFields[.email], self.verificaEmail(email.text!) else { return false }
         
         return true
         
